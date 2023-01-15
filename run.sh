805a764d0145459456d0f94e5f01dbac604538df
Andrew Dawes <andrewjamesdawes@gmail.com>
2023-01-15 18:19:34 -0500

    Run as user who is added to docker group with access to daemon socket

--- run.sh
+++ run.sh
@@ -6,8 +6,17 @@ fi
 # Please note that this container has access to the host's docker daemon
 # https://itnext.io/docker-in-docker-521958d34efd
 
+# Get the group that owns the docker daemon socket and add this group to container's user
+# https://forums.docker.com/t/docker-in-docker-for-non-root-user/70150
+DOCKER_SOCKET=/var/run/docker.sock
+
+if [ -S ${DOCKER_SOCKET} ]; then
+    DOCKER_GID=$(stat -c '%g' ${DOCKER_SOCKET})
+fi
+
 eval_cmd="docker run \
 -d \
+--group-add ${DOCKER_GID} \
 --restart=unless-stopped \
 --name $PROJECT_DOCKER_CONTAINER_NAME \
 --env ORGANIZATION=$PROJECT_GITHUB_ORGANIZATION \
